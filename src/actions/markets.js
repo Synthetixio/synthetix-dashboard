ccc22507da1eb20e2ea4a190912fceccbe29eab3
Clement Balestrat <>
2018-10-12 13:12:00 +1100

    change markets URL to staging

--- src/actions/markets.js
+++ src/actions/markets.js
@@ -1,106 +1,101 @@
 import {
-    FETCH_HAV_CURRENCY,
-    FETCH_HAV_CURRENCY_SUCCESS,
-    FETCH_HAV_CURRENCY_ERROR,
-    FETCH_NUSD_CURRENCY,
-    FETCH_NUSD_CURRENCY_SUCCESS,
-    FETCH_NUSD_CURRENCY_ERROR,
-    FETCH_COINMARKETCAP_HAV,
-    FETCH_COINMARKETCAP_HAV_SUCCESS,
-    FETCH_COINMARKETCAP_HAV_ERROR,
-    FETCH_COINMARKETCAP_NUSD,
-    FETCH_COINMARKETCAP_NUSD_SUCCESS,
-    FETCH_COINMARKETCAP_NUSD_ERROR
-} from './actionTypes.js'
+  FETCH_HAV_CURRENCY,
+  FETCH_HAV_CURRENCY_SUCCESS,
+  FETCH_HAV_CURRENCY_ERROR,
+  FETCH_NUSD_CURRENCY,
+  FETCH_NUSD_CURRENCY_SUCCESS,
+  FETCH_NUSD_CURRENCY_ERROR,
+  FETCH_COINMARKETCAP_HAV,
+  FETCH_COINMARKETCAP_HAV_SUCCESS,
+  FETCH_COINMARKETCAP_HAV_ERROR,
+  FETCH_COINMARKETCAP_NUSD,
+  FETCH_COINMARKETCAP_NUSD_SUCCESS,
+  FETCH_COINMARKETCAP_NUSD_ERROR,
+} from './actionTypes.js';
 
 export const fetchHAV = () => ({
   type: FETCH_HAV_CURRENCY,
-  url: "https://api.coinmarketcap.com/v2/ticker/2586/",
+  url: 'https://api.coinmarketcap.com/v2/ticker/2586/',
   success: handleFetchHAVSuccess,
-  error: handleFetchHAVError
+  error: handleFetchHAVError,
 });
 
-export const handleFetchHAVSuccess = (data) => {
+export const handleFetchHAVSuccess = data => {
   return {
     type: FETCH_HAV_CURRENCY_SUCCESS,
-    data
-  }
-}
+    data,
+  };
+};
 
-export const handleFetchHAVError = (error) => {
+export const handleFetchHAVError = error => {
   return {
     type: FETCH_HAV_CURRENCY_ERROR,
-    error
-  }
-}
+    error,
+  };
+};
 
 /////////////////////////////////////////////
-export const fetchNUSD = (data) => ({
+export const fetchNUSD = data => ({
   type: FETCH_NUSD_CURRENCY,
-  url: "https://api.coinmarketcap.com/v2/ticker/2927/",
+  url: 'https://api.coinmarketcap.com/v2/ticker/2927/',
   success: handleFetchNUSDSuccess,
-  error: handleFetchNUSDError
+  error: handleFetchNUSDError,
 });
 
-export const handleFetchNUSDSuccess = (data) => {
+export const handleFetchNUSDSuccess = data => {
   return {
     type: FETCH_NUSD_CURRENCY_SUCCESS,
-    data
-  }
-}
+    data,
+  };
+};
 
-export const handleFetchNUSDError = (error) => {
+export const handleFetchNUSDError = error => {
   return {
     type: FETCH_NUSD_CURRENCY_ERROR,
-    error
-  }
-}
+    error,
+  };
+};
 
 /////////////////////////////////////////////
-export const fetchCoinmarketcapHAV = (data) => ({
+export const fetchCoinmarketcapHAV = data => ({
   type: FETCH_COINMARKETCAP_HAV,
-  url: "https://havven.io/api/markets",
+  url: 'http://web-staging.havven.io/api/markets',
   success: handleFetchCoinmarketcapHAVSuccess,
-  error: handleFetchCoinmarketcapHAVError
+  error: handleFetchCoinmarketcapHAVError,
 });
 
-export const handleFetchCoinmarketcapHAVSuccess = (data) => {
-
+export const handleFetchCoinmarketcapHAVSuccess = data => {
   return {
     type: FETCH_COINMARKETCAP_HAV_SUCCESS,
-    data
-  }
-}
+    data,
+  };
+};
 
-export const handleFetchCoinmarketcapHAVError = (error) => {
-
+export const handleFetchCoinmarketcapHAVError = error => {
   return {
     type: FETCH_COINMARKETCAP_HAV_ERROR,
-    error
-  }
-}
+    error,
+  };
+};
 
 /////////////////////////////////////////////
-export const fetchCoinmarketcapNUSD = (data) => ({
+export const fetchCoinmarketcapNUSD = data => ({
   type: FETCH_COINMARKETCAP_NUSD,
-  //url: "https://havven.io/api/marketsNusd",
-  url: "http://web-staging.havven.io/api/marketsnusd",
+  url: 'http://web-staging.havven.io/api/marketsnusd',
   success: handleFetchCoinmarketcapNUSDSuccess,
-  error: handleFetchCoinmarketcapNUSDError
+  error: handleFetchCoinmarketcapNUSDError,
 });
 
-export const handleFetchCoinmarketcapNUSDSuccess = (data) => {
-
+export const handleFetchCoinmarketcapNUSDSuccess = data => {
   return {
     type: FETCH_COINMARKETCAP_NUSD_SUCCESS,
-    data
-  }
-}
+    data,
+  };
+};
 
-export const handleFetchCoinmarketcapNUSDError = (error) => {
-
+export const handleFetchCoinmarketcapNUSDError = error => {
   return {
     type: FETCH_COINMARKETCAP_NUSD_ERROR,
-    error
-  }
-}
+    error,
+  };
+};

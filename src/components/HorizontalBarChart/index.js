f268c438f5bf471fde4311ce6ac35c6d19c2fc7f
Clement Balestrat <>
2019-06-19 17:01:10 +1000

    openInterest chart using $ scale

--- src/components/HorizontalBarChart/index.js
+++ src/components/HorizontalBarChart/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import numeral from 'numeral';
 import './styles.sass';
 
 class HorizontalBarChart extends Component {
   render() {
     const { data } = this.props;
+    const max = data.reduce((acc, curr) => {
+      if (curr.z > acc) acc = curr.z;
+      return acc;
+    }, 0);
     return (
       <div className="horizontalBarChart">
         <div className="legend">
@@ -16,17 +21,19 @@ class HorizontalBarChart extends Component {
             <div className="barWrapper">
               <div
                 style={{
-                  left: `calc(100% - ${(100 * synth.y) / synth.z}% + 1px)`,
+                  left: `calc(100% - ${(100 * synth.y) / max}% + 1px)`,
                 }}
                 className="bar"
               >
-                <div className="synthLabel">{synth.label}</div>
+                <div className="synthLabel">{`${synth.label} (${numeral(
+                  synth.z
+                ).format('$0,(0)')} total)`}</div>
                 <div
-                  style={{ width: (100 * synth.y) / synth.z + '%' }}
+                  style={{ width: (100 * synth.y) / max + '%' }}
                   className="inverseSynthBar"
                 />
                 <div
-                  style={{ width: (100 * synth.x) / synth.z + '%' }}
+                  style={{ width: (100 * synth.x) / max + '%' }}
                   className="synthBar"
                 />
               </div>

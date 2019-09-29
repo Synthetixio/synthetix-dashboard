f1b391b312cf461676e0d37b799155039fe67a7b
justin j. moses <justinjmoses@gmail.com>
2019-09-29 10:37:08 -0400

    Use TheGraph for exchange volume and fees

--- src/config/sagas.js
+++ src/config/sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put, all } from 'redux-saga/effects';
+import { takeEvery, call, put, all } from "redux-saga/effects";
 
 import {
   FETCH_CHARTS,
@@ -14,16 +14,18 @@ import {
   FETCH_EXCHANGE_TICKER,
   FETCH_EXCHANGE_TICKER_SUCCESS,
   FETCH_UNISWAP_POOL,
-  FETCH_UNISWAP_POOL_SUCCESS,
-} from '../actions/actionTypes';
+  FETCH_UNISWAP_POOL_SUCCESS
+} from "../actions/actionTypes";
 
-import { doFetch } from './api';
+import { doFetch } from "./api";
 
-let apiUri = process.env.API_URL || 'https://api.synthetix.io/api/';
+import { utils } from "ethers";
+
+let apiUri = process.env.API_URL || "https://api.synthetix.io/api/";
 
 //CHARTS
 function* fetchCharts() {
-  const fetchUri = apiUri + 'dataPoint/chartData';
+  const fetchUri = apiUri + "dataPoint/chartData";
   const data = yield call(doFetch, fetchUri);
   yield put({ type: FETCH_CHARTS_SUCCESS, payload: { data } });
 }
@@ -34,25 +36,85 @@ function* fetchChartsCall() {
 
 // EXCHANGE
 function* fetchExchangeOpenInterest() {
-  const fetchUri = apiUri + 'exchange/openInterest';
+  const fetchUri = apiUri + "exchange/openInterest";
   const data = yield call(doFetch, fetchUri);
   yield put({ type: FETCH_OPEN_INTEREST_SUCCESS, payload: { data } });
 }
 
+const graphAPI =
+  "https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix-exchanges";
+const PAGE_SIZE = 100;
+
+function* pageResults({ queryCreator, field }) {
+  function* runner({ skip }) {
+    let response = yield doFetch(
+      graphAPI,
+      "POST",
+      queryCreator({ skip }),
+      undefined,
+      false
+    );
+
+    let { [field]: results } = response.body.data;
+
+    if (results.length >= PAGE_SIZE) {
+      results = results.concat(yield runner({ skip: skip + PAGE_SIZE }));
+    }
+
+    return results;
+  }
+
+  return yield runner({ skip: 0 });
+}
+
 function* fetchExchangeTradingVolume() {
-  const fetchUri = apiUri + 'exchange/volume';
-  const data = yield call(doFetch, fetchUri);
-  yield put({ type: FETCH_TRADING_VOLUME_SUCCESS, payload: { data } });
+  const ts = Math.floor(Date.now() / 1e3);
+  const oneDayAgo = ts - 3600 * 24;
+
+  const synthExchangesIn24Hrs = yield call(pageResults, {
+    field: "synthExchanges",
+    queryCreator: ({ skip }) =>
+      `{"query":"{synthExchanges(first:${PAGE_SIZE}, skip:${skip}, orderBy:timestamp, orderDirection:desc, where:{timestamp_gt: ${oneDayAgo}}){id,from,gasPrice,fromAmount,fromCurrencyKey,toCurrencyKey,amountInUSD,block,timestamp,toAddress}}","variables":null}`
+  });
+
+  const last24Hours = synthExchangesIn24Hrs.reduce(
+    (memo, { amountInUSD }) => memo + Number(utils.formatEther(amountInUSD)),
+    0
+  );
+
+  const [{ totalFeesGeneratedInUSD, exchangeUSDTally }] = yield call(
+    pageResults,
+    {
+      field: "totals",
+      queryCreator: () =>
+        `{"query": "{totals(first: 1) {exchangers,exchangeUSDTally,totalFeesGeneratedInUSD}}", "variables": null}`
+    }
+  );
+
+  const data = {
+    body: {
+      totalFeesGenerated: Number(utils.formatEther(totalFeesGeneratedInUSD)),
+      volume: {
+        last24Hours,
+        total: Number(utils.formatEther(exchangeUSDTally))
+      }
+    }
+  };
+
+  yield put({
+    type: FETCH_TRADING_VOLUME_SUCCESS,
+    payload: { data }
+  });
 }
 
 function* fetchExchangeTicker() {
-  const fetchUri = apiUri + 'exchange/ticker/seth-susd';
+  const fetchUri = apiUri + "exchange/ticker/seth-susd";
   const data = yield call(doFetch, fetchUri);
   yield put({ type: FETCH_EXCHANGE_TICKER_SUCCESS, payload: { data } });
 }
 
 function* fetchUniswapPool() {
-  const fetchUri = apiUri + 'exchange/uniswap/seth';
+  const fetchUri = apiUri + "exchange/uniswap/seth";
   const data = yield call(doFetch, fetchUri);
   yield put({ type: FETCH_UNISWAP_POOL_SUCCESS, payload: { data } });
 }
@@ -109,7 +171,7 @@ const rootSaga = function*() {
     fetchOpenInterest(),
     fetchTradingVolume(),
     fetchExchangeTickerCall(),
-    fetchUniswapPoolCall(),
+    fetchUniswapPoolCall()
   ]);
 };
 

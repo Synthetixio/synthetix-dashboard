9102fa5542938af06aae2ef62078aab3ad13a003
Clement Balestrat <clement@synthetix.io>
2020-03-12 19:09:28 +1100

    adding dashboardData endpoint

--- src/reducers/charts.js
+++ src/reducers/charts.js
@@ -8,34 +8,11 @@ const chartTypes = [
 	'NominPrice',
 	'HavvenVolume24h',
 	'NominVolume24h',
-	'NominFeesCollected',
-	'UnlockedHavUsdBalance',
-	'LockedHavUsdBalance',
-	'LockedHavRatio',
-	'UnlockedHavBalance',
-	'LockedHavBalance',
-	'NetworkCollateralizationRatio',
-	'ActiveCollateralizationRatio',
 ];
 
-const chartTypesHAV = [
-	'HavvenMarketCap',
-	'HavvenPrice',
-	'HavvenVolume24h',
-	'UnlockedHavBalance',
-	'LockedHavBalance',
-	'LockedHavUsdBalance',
-	'LockedHavRatio',
-];
+const chartTypesHAV = ['HavvenMarketCap', 'HavvenPrice', 'HavvenVolume24h'];
 
-const chartTypesNomin = [
-	'NominMarketCap',
-	'NominPrice',
-	'NominVolume24h',
-	'NominFeesCollected',
-	'NetworkCollateralizationRatio',
-	'ActiveCollateralizationRatio',
-];
+const chartTypesNomin = ['NominMarketCap', 'NominPrice', 'NominVolume24h'];
 
 const initialState = {
 	stats: {},
@@ -55,86 +32,95 @@ chartTypes.forEach(type => (initialState[type] = {}));
 
 export default (state = initialState, action) => {
 	switch (action.type) {
-	case FETCH_CHARTS_SUCCESS:
-		try {
-			const chartData = chartTypes
-				.map(type => {
-					const period = getInitialPeriod(type);
-					return {
-						[type]: parseChartData(action.payload.data.body[type].data, type, period),
-					};
-				})
-				.reduce((acc, next) => ({ ...acc, ...next }), {});
-			const data = action.payload.data.body;
-			return {
-				...chartData,
-				sourceData: data,
-				havPeriod: state.havPeriod,
-				nUSDPeriod: state.nUSDPeriod,
-				lastUpdated: data.HavvenMarketCap.data[data.HavvenMarketCap.data.length - 1].created,
-				stats: {
-					havvenMarketCap24hDelta: data.HavvenMarketCap.usd24hDelta,
-					havvenPriceCap24hDelta: data.HavvenPrice.usd24hDelta,
-					havvenVolume24hDelta: data.HavvenVolume24h.usd24hDelta,
-					nominMarketCap24hDelta: data.NominMarketCap.usd24hDelta,
-					nominPriceCap24hDelta: data.NominPrice.usd24hDelta,
-					nominVolume24hDelta: data.NominVolume24h.usd24hDelta,
-					havvenMarketCap:
-							data.HavvenMarketCap.data[data.HavvenMarketCap.data.length - 1].usdValue,
-					havvenPriceCap: data.HavvenPrice.data[data.HavvenPrice.data.length - 1].usdValue,
-					havvenVolume24h:
-							data.HavvenVolume24h.data[data.HavvenVolume24h.data.length - 1].usdValue,
-					unlockedHavUsdBalance:
-							data.UnlockedHavUsdBalance.data[data.UnlockedHavUsdBalance.data.length - 1].usdValue,
-					lockedHavUsdBalance:
-							data.LockedHavUsdBalance.data[data.LockedHavUsdBalance.data.length - 1].usdValue,
-					lockedHavRatio: data.LockedHavRatio.data[data.LockedHavRatio.data.length - 1].usdValue,
-					nominMarketCap: data.NominMarketCap.data[data.NominMarketCap.data.length - 1].usdValue,
-					nominPriceCap: data.NominPrice.data[data.NominPrice.data.length - 1].usdValue,
-					nominVolume24h: data.NominVolume24h.data[data.NominVolume24h.data.length - 1].usdValue,
-					nominFeesCollected:
-							data.NominFeesCollected.data[data.NominFeesCollected.data.length - 1].usdValue,
-					networkCollateralizationRatio:
-							data.NetworkCollateralizationRatio.data[
-								data.NetworkCollateralizationRatio.data.length - 1
+		case FETCH_CHARTS_SUCCESS:
+			try {
+				let { chartHistoricalData, dashboardData } = action.payload;
+				const chartData = chartTypes
+					.map(type => {
+						const period = getInitialPeriod(type);
+						return {
+							[type]: parseChartData(chartHistoricalData.body[type].data, type, period),
+						};
+					})
+					.reduce((acc, next) => ({ ...acc, ...next }), {});
+				chartHistoricalData = chartHistoricalData.body;
+				dashboardData = dashboardData.body;
+				return {
+					...chartData,
+					sourceData: chartHistoricalData,
+					havPeriod: state.havPeriod,
+					nUSDPeriod: state.nUSDPeriod,
+					lastUpdated:
+						chartHistoricalData.HavvenMarketCap.data[
+							chartHistoricalData.HavvenMarketCap.data.length - 1
+						].created,
+					stats: {
+						havvenMarketCap24hDelta: chartHistoricalData.HavvenMarketCap.usd24hDelta,
+						havvenPriceCap24hDelta: chartHistoricalData.HavvenPrice.usd24hDelta,
+						havvenVolume24hDelta: chartHistoricalData.HavvenVolume24h.usd24hDelta,
+						nominMarketCap24hDelta: chartHistoricalData.NominMarketCap.usd24hDelta,
+						nominPriceCap24hDelta: chartHistoricalData.NominPrice.usd24hDelta,
+						nominVolume24hDelta: chartHistoricalData.NominVolume24h.usd24hDelta,
+						havvenMarketCap:
+							chartHistoricalData.HavvenMarketCap.data[
+								chartHistoricalData.HavvenMarketCap.data.length - 1
 							].usdValue,
-					activeCollateralizationRatio:
-							data.ActiveCollateralizationRatio.data[
-								data.ActiveCollateralizationRatio.data.length - 1
+						havvenPriceCap:
+							chartHistoricalData.HavvenPrice.data[chartHistoricalData.HavvenPrice.data.length - 1]
+								.usdValue,
+						havvenVolume24h:
+							chartHistoricalData.HavvenVolume24h.data[
+								chartHistoricalData.HavvenVolume24h.data.length - 1
 							].usdValue,
-				},
-			};
-		} catch (e) {
-			console.log('error', e);
+						lockedHavUsdBalance: dashboardData.LockedHavUsdBalance,
+						lockedHavRatio: dashboardData.LockedHavRatio,
+						nominMarketCap:
+							chartHistoricalData.NominMarketCap.data[
+								chartHistoricalData.NominMarketCap.data.length - 1
+							].usdValue,
+						nominPriceCap:
+							chartHistoricalData.NominPrice.data[chartHistoricalData.NominPrice.data.length - 1]
+								.usdValue,
+						nominVolume24h:
+							chartHistoricalData.NominVolume24h.data[
+								chartHistoricalData.NominVolume24h.data.length - 1
+							].usdValue,
+						nominFeesCollected: dashboardData.NominFeesCollected,
+						networkCollateralizationRatio: dashboardData.NetworkCollateralizationRatio,
+						activeCollateralizationRatio: dashboardData.ActiveCollateralizationRatio,
+					},
+				};
+			} catch (e) {
+				console.log('error', e);
+				return state;
+			}
+
+		case SET_PERIOD_CHART:
+			try {
+				const { token, period } = action;
+				if (!token || !period) throw 'Error: Set chart period parameter missing!';
+				const types = token === 'HAV' ? chartTypesHAV : chartTypesNomin;
+				const havPeriod = token === 'HAV' ? period : state.havPeriod;
+				const nUSDPeriod = token === 'nUSD' ? period : state.nUSDPeriod;
+
+				const chartData = types
+					.map(type => ({
+						[type]: parseChartData(state.sourceData[type].data, type, period),
+					}))
+					.reduce((acc, next) => ({ ...acc, ...next }), {});
+
+				return {
+					...state,
+					...chartData,
+					havPeriod,
+					nUSDPeriod,
+				};
+			} catch (e) {
+				console.log('error', e);
+				return state;
+			}
+
+		default:
 			return state;
-		}
-
-	case SET_PERIOD_CHART:
-		try {
-			const { token, period } = action;
-			if (!token || !period) throw 'Error: Set chart period parameter missing!';
-			const types = token === 'HAV' ? chartTypesHAV : chartTypesNomin;
-			const havPeriod = token === 'HAV' ? period : state.havPeriod;
-			const nUSDPeriod = token === 'nUSD' ? period : state.nUSDPeriod;
-
-			const chartData = types
-				.map(type => ({
-					[type]: parseChartData(state.sourceData[type].data, type, period),
-				}))
-				.reduce((acc, next) => ({ ...acc, ...next }), {});
-
-			return {
-				...state,
-				...chartData,
-				havPeriod,
-				nUSDPeriod,
-			};
-		} catch (e) {
-			console.log('error', e);
-			return state;
-		}
-
-	default:
-		return state;
 	}
 };
